2b6e8dcade860fa613e5f345b1b27d15e9693690
Egor Akimov <akimoved@gmail.com>
2024-05-24 18:20:25 +0400

    feat add search by title, length and year

--- repo.py
+++ repo.py
@@ -13,8 +13,60 @@ class Movie:
         self.studio = studio
         self.actors = actors
 
+    def check_match(self, filter: dict[str]) -> bool:
+        '''
+        Check whether given movie matches all search criteria
+        '''
+        if not self.check_title(filter["title"]):
+            return False
+        if not self.check_year(filter["year"]):
+            return False
+        if not self.check_length(filter["length"]):
+            return False
+
+        return True
+
+    def check_title(self, match_title: str) -> bool:
+        return True if (not match_title) or (match_title.lower() in self.title.lower()) else False
+
+    def check_year(self, match_year: datetime.date) -> bool:
+        return True if (not match_year) or (self.year <= match_year) else False
+    
+    def check_length(self, match_length: tuple) -> bool:
+        if not match_length:
+            return True
+        
+        comparison: str = match_length[0]
+        search_length: int = match_length[1]
+        match comparison:
+            case '<':
+                if self.length < search_length:
+                    return True
+            case '<=':
+                if self.length <= search_length:
+                    return True
+            case '>':
+                if self.length > search_length:
+                    return True
+            case '>=':
+                if self.length >= search_length:
+                    return True
+            case '=':
+                if self.length == search_length:
+                    return True
+                
+        return False
+
+    @staticmethod
+    def from_csv(row: list[str]):
+        row[1] = row[1].split(';')
+        row[3] = datetime.date(int(row[3]), 1, 1)
+        row[4] = int(row[4])
+        row[6] = row[6].split(';')
+        return Movie(*row)
+
     def __str__(self) -> str:
-        return f"{self.title}, {self.year}"
+        return f"{self.title}, {self.year.year}"
 
 
 class MovieRepo:
@@ -30,37 +82,43 @@ class MovieRepo:
         with open(self.src, encoding="utf-8") as f:
             rows = reader(f)
             for row in rows:
-                # REDO IMMEDIATELY
-                movie = Movie(*row)
-                movie.genre = movie.genre.split(';')
-                movie.year = datetime.date(int(movie.year), 1, 1)
-                movie.length = int(movie.length)
-                movie.actors = movie.actors.split(';')
-                self.database.append(movie)
+                self.database.append(Movie.from_csv(row))
 
     def get_all_movies(self) -> list[Movie]:
         return self.database
 
     def get_movies_by(self, filter: dict[str, str]) -> list[Movie]:
+        res: list[Movie] = []
         if len(filter) == 0:
-            return []
-            
+            return res
+
         filter = MovieRepo.create_filter(filter)
-        
+        print(filter)
+
         for movie in self.database:
-            print("")
-            for item in zip(filter.values(),movie.__dict__.values()):
-                print(item)
-        
-        return []
-    
+            if movie.check_match(filter):
+                res.append(movie)
+
+        return res
+
     @staticmethod
     def create_filter(filter: dict[str, str]) -> dict[str]:
-        filter['genre'] = [genre.strip() for genre in filter['genre'].split(',')] if filter['genre'] else ''
-        filter['actors'] = [actor.strip() for actor in filter['actors'].split(',')] if filter['actors'] else ''
+        filter['genre'] = [genre.strip()
+                           for genre in filter['genre'].split(',')] if filter['genre'] else ''
+        
+        filter['year'] = datetime.date(int(filter['year']), 1, 1) if filter['year'] else ''
+        
+        if filter['length']:
+            filter['length'] = filter['length'].split()
+            filter['length'][1] = int(filter['length'][1])
+            filter['length'] = tuple(filter['length'])
+        
+        filter['actors'] = [actor.strip() for actor in filter['actors'].split(
+            ',')] if filter['actors'] else ''
         return filter
-    
-    
-repo = MovieRepo("movies.csv")
-filter = {'title': 'godfather', 'genre': 'drama, comedy', 'producer': '', 'year': '', 'length': '', 'studio': '', 'actors': 'actor1, actor2'}
-repo.get_movies_by(filter)
+
+
+# repo = MovieRepo("movies.csv")
+# filter = {'title': '', 'genre': '', 'producer': '',
+#           'year': '', 'length': '> 180', 'studio': '', 'actors': ''}
+# print(*repo.get_movies_by(filter))
--- view.py
+++ view.py
@@ -21,7 +21,7 @@ class MovieView:
         
         print("\nEnter search options","\ns to stop")
         for i in range(len(allowed_fields)):
-            value = input(f"Enter value for field '{allowed_fields[i]}. Use , for multiple options': ")
+            value = input(f"Enter value for field '{allowed_fields[i]}'. Use , for multiple options: ")
             if value == 's':
                 for j in range(i, len(allowed_fields)):
                     search_filter[allowed_fields[j]] = ""
